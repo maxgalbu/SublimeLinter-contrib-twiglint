97a57e3f582fc0460a056d67b531e9e229824be9
Max Galbusera <max.galbusera@gmail.com>
2014-12-17 18:02:55 +0100

    import

---
+++ linter.py
@@ -0,0 +1,30 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by m.galbusera
+# Copyright (c) 2014 m.galbusera
+#
+# License: MIT
+#
+
+"""This module exports the Twiglint plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Twiglint(Linter):
+
+    """Provides an interface to twiglint."""
+
+    syntax = ('twig', 'html (twig)', 'html')
+    cmd = 'twig-lint lint'
+    version_args = '--version'
+    version_re = r'version v(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0.1'
+    regex = (
+        r'(?ism)KO in [^(]+\(line (?P<line>\d+)\).+?>> \d+ .+?>> (?P<message>[^\r\n]+)'
+    )
+    multiline = True
+    line_col_base = (1, 1)
+    tempfile_suffix = 'twig'
